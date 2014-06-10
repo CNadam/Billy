afa5ac8f68ea07e81acdbc5fa6e7fbc2cd3da374
Vibin reddy <v@vibinreddy.me>
2014-06-10 20:39:38 +0530

    just some cleanup

--- billy/src/main/java/com/vibin/billy/Fragment1.java
+++ billy/src/main/java/com/vibin/billy/Fragment1.java
@@ -3,8 +3,6 @@ package com.vibin.billy;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.util.LruCache;
@@ -12,12 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -39,38 +35,40 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Fragment1 extends Fragment {
     ArrayList<BillyData> mData;
-    String[] song;
-    String[] album;
-    String[] artist;
-    int[] artwork;
+    String[] billySong;
     ListView lv;
     View v;
     ImageLoader imgload;
     MyCustomAdapter myCustomAdapter;
     RequestQueue req;
-    String searchparam = "dark+horse";
+    String searchparam;
+    JSONArray mJsonArray;
     String uri = "http://itunes.apple.com/search?term=" + searchparam + "&limit=1";
     String rssurl ="http://www1.billboard.com/rss/charts/hot-100";
-    int mIndex = 0;
-    int nIndex = 0;
-
-    private static final String TAG = "Fragment1";
+    String trackName,artistName,collectionName,artworkUrl;
+    int mIndex, nIndex = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mData = new ArrayList<BillyData>(25);
+        mData = new ArrayList<BillyData>(24);
+        while(mData.size() < 24) {
+            mData.add(new BillyData());
+        }
 
         // Get Billboard XML
+        billySong = new String[24];
         req = Volley.newRequestQueue(getActivity());
         StringRequest stringreq = new StringRequest(Request.Method.GET,rssurl,billyComplete(),billyError());
 
+        // Cache for images
         imgload = new ImageLoader(req, new ImageLoader.ImageCache() {
-            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(24);
             public void putBitmap(String url, Bitmap bitmap) {
                 mCache.put(url, bitmap);
             }
@@ -100,11 +98,8 @@ public class Fragment1 extends Fragment {
 
     public class BillyData {
         String song, album, artist, artwork;
-        BillyData(String song) {
-            this.song = song;
-        }
-
-        public void setItunes(String album, String artist, String artwork, String song){
+        BillyData() {}
+        BillyData(String album, String artist, String artwork, String song) {
             this.album = album;
             this.artist = artist;
             this.artwork = artwork;
@@ -112,8 +107,7 @@ public class Fragment1 extends Fragment {
         }
     }
 
-
-
+    // Listeners for Billboard request
     private Response.Listener<String> billyComplete() {
         return new Response.Listener<String>() {
             @Override
@@ -126,7 +120,6 @@ public class Fragment1 extends Fragment {
             }
         };
     }
-
 
     private Response.ErrorListener billyError() {
         return new Response.ErrorListener() {
@@ -141,16 +134,12 @@ public class Fragment1 extends Fragment {
 
     // Parse the XML as InputStream
     private void handleXML(String response) throws IOException {
-        mData = new ArrayList<BillyData>(100);
         try {
             InputStream in = null;
-            try {
-                in = IOUtils.toInputStream(response, "UTF-8");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            XmlPullParserFactory xmlppf = XmlPullParserFactory.newInstance();
-            XmlPullParser parser = xmlppf.newPullParser();
+            in = IOUtils.toInputStream(response, "UTF-8");
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
             parser.setInput(in,null);
 
             int event = parser.getEventType();
@@ -164,11 +153,8 @@ public class Fragment1 extends Fragment {
 
                         if(parser.next() == XmlPullParser.TEXT) {
                             if (i != 0) {
-                                String title = parser.getText();
-
-                                BillyData obj = new BillyData(cleanSongTitle(parser.getText()));
-                                mData.add(i-1, obj);
-                                Log.d(getClass().getName(), mData.get(i-1).song);
+                                billySong[i-1] = cleanSongTitle(parser.getText());
+                                Log.d(getClass().getName(), billySong[i-1]);
                             }
                             i++;
                         }
@@ -179,22 +165,18 @@ public class Fragment1 extends Fragment {
                     Log.d(getClass().getName(), "Parsed 200 lines!");
                     break;
                 }
-                try {
-                    event = parser.next();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+                event = parser.next();
             }
 
-
-            Log.d(getClass().getName(), "Parsing is done and size of arraylist is "+mData.size());
-            //Query iTunes for first Billboard song
-            searchparam = mData.get(mIndex).song.replaceAll(" ","+"); // Put Billboard song as iTunes search parameter
-            mIndex++;
-            uri = "http://itunes.apple.com/search?term=" + searchparam + "&limit=1";
-            JsonObjectRequest jsonreq =  new JsonObjectRequest(Request.Method.GET,uri,null, itunesComplete(), itunesError());
-            req.add(jsonreq);
+            while(mIndex < billySong.length-1)
+            {
+                searchparam = billySong[mIndex].replaceAll(" ","+"); // Put Billboard song as iTunes search parameter
+                uri = "http://itunes.apple.com/search?term=" + searchparam + "&limit=1";
+                JsonObjectRequest jsonreq =  new JsonObjectRequest(Request.Method.GET,uri,null, itunesComplete(), itunesError());
+                req.add(jsonreq);
+                mIndex++;
+            }
+            Log.d(getClass().getName(), "itunes requests complete!");
 
         } catch (XmlPullParserException e) {
             e.printStackTrace();
@@ -203,7 +185,7 @@ public class Fragment1 extends Fragment {
 
     // Manipulating the Billboard Song string
     private String cleanSongTitle(String text) {
-        return  text.substring(text.indexOf(":") + 2, text.indexOf(","));
+       return  text.substring(text.indexOf(":") + 2, text.indexOf(","));
     }
 
 
@@ -212,37 +194,30 @@ public class Fragment1 extends Fragment {
             @Override
             public void onResponse(JSONObject jsonObject) {
                 try {
-                    String trackName,artistName,collectionName,artworkUrl;
-                    JSONArray mJsonArray = jsonObject.getJSONArray("results");
+                    mJsonArray = jsonObject.getJSONArray("results");
                     artistName = mJsonArray.getJSONObject(0).getString("artistName");
                     collectionName = mJsonArray.getJSONObject(0).getString("collectionName");
-                    artworkUrl = mJsonArray.getJSONObject(0).getString("artworkUrl100");
+                    artworkUrl = mJsonArray.getJSONObject(0).getString("artworkUrl100").replaceAll("100x100","400x400");
                     trackName = mJsonArray.getJSONObject(0).getString("trackName");
-                    // Log.d(getClass().getName(), artistName+ " "+ collectionName +" "+ artworkUrl);
 
-                    String newUrl = artworkUrl.replaceAll("100x100","400x400");
                     String newTrack;
                     if(trackName.contains("("))
                     {
-                        newTrack = trackName.substring(0,trackName.indexOf("("));
+                        newTrack = capitalize(trackName.substring(0, trackName.indexOf("("))).trim();
                     }
                     else
                     {
-                        newTrack = trackName;
+                        newTrack = capitalize(trackName).trim();
                     }
-                    Log.d(getClass().getName(), newTrack);
-                    mData.get(nIndex).setItunes(collectionName, artistName, newUrl, newTrack);
+
+                    int match = Arrays.asList(billySong).indexOf(newTrack);
+                    Log.d(getClass().getName(), newTrack+":  match is "+match);
+                    mData.add(match, new BillyData(collectionName, artistName, artworkUrl, newTrack));
+                    //mData.add(match, obj);
+
                    // Log.d(getClass().getName(), nIndex + " " + mData.get(nIndex).song + mData.get(nIndex).artist + mData.get(nIndex).album + mData.get(nIndex).song);
-                    nIndex++;
+                   // nIndex++;
                     myCustomAdapter.notifyDataSetChanged();
-                    if(mIndex<24) {
-                        //Log.d(getClass().getName(), mData.get(mIndex).song);
-                        searchparam = mData.get(mIndex).song.replaceAll(" ", "+"); // Put Billboard song as iTunes search parameter
-                        uri = "http://itunes.apple.com/search?term=" + searchparam + "&limit=1";
-                        JsonObjectRequest jsonreq = new JsonObjectRequest(Request.Method.GET, uri, null, itunesComplete(), itunesError());
-                        req.add(jsonreq);
-                        mIndex++;
-                    }
                 } catch (JSONException e) {
                     Log.d(getClass().getName(), e+ "mIndex is" + mIndex);
                 }
@@ -260,16 +235,27 @@ public class Fragment1 extends Fragment {
         };
     }
 
+    private String capitalize(String text) {
+        String[] words = text.split(" ");
+        StringBuilder sb = new StringBuilder();
+        if (words[0].length() > 0) {
+            sb.append(Character.toUpperCase(words[0].charAt(0)) + words[0].subSequence(1, words[0].length()).toString().toLowerCase());
+            for (int i = 1; i < words.length; i++) {
+                sb.append(" ");
+                sb.append(Character.toUpperCase(words[i].charAt(0)) + words[i].subSequence(1, words[i].length()).toString().toLowerCase());
+            }
+        }
+        //Log.d(getClass().getName(), "The capitalized string is"+sb.toString());
+        return sb.toString();
+    }
 
 
-
+    //Adapter for the ListView
     class MyCustomAdapter extends BaseAdapter {
         Context c;
 
         MyCustomAdapter(Context c) {
-            int i;
             this.c = c;
-
             Log.d(getClass().getName(), "This is the adapter constructor!");
 
 /*
@@ -316,7 +302,7 @@ public class Fragment1 extends Fragment {
         @Override
         public View getView(int i, View convertView, ViewGroup viewGroup) {
             View row =  convertView;
-            MyViewHolder holder = null;
+            MyViewHolder holder;
             if(row == null) {
                 LayoutInflater lif = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 row = lif.inflate(R.layout.single_row, viewGroup, false);
@@ -330,6 +316,7 @@ public class Fragment1 extends Fragment {
 
             //put an object item from ArrayList in BillyData object
             BillyData temp = mData.get(i);
+            Log.d(getClass().getName(), i+ " "+temp.album + " "+temp.artist + " "+temp.song + " "+temp.artwork);
 
             //set everything
             holder.album.setText(temp.album);
@@ -337,7 +324,7 @@ public class Fragment1 extends Fragment {
             holder.song.setText(temp.song);
             holder.artwork.setImageUrl(temp.artwork,imgload);
 
-         //   Log.d(getClass().getName(), temp.album + temp.artist + temp.song + temp.artwork);
+
             return row;
         }
     }
