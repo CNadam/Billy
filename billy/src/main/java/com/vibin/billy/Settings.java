99398351f3979ba57618be6136f9b1f1915d76b1
Vibin reddy <v@vibinreddy.me>
2014-11-06 14:01:16 +0530

    handle onClicks for new settings

--- billy/src/main/java/com/vibin/billy/Settings.java
+++ billy/src/main/java/com/vibin/billy/Settings.java
@@ -1,13 +1,17 @@
 package com.vibin.billy;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
+import android.app.FragmentManager;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
@@ -64,12 +68,56 @@ public class Settings extends ActionBarActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragment {
-        SharedPreferences pref;
+
+        Preference rate,changelog,licenses;
+        FragmentManager fm;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings);
+            fm = getActivity().getFragmentManager();
+            rate = getPreferenceScreen().findPreference("rate");
+            changelog = getPreferenceScreen().findPreference("changelog");
+            licenses = getPreferenceScreen().findPreference("licenses");
+            setOnClickListeners();
+        }
+
+        private void setOnClickListeners() {
+            rate.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Uri uri = Uri.parse("market://details?id=" + getActivity().getPackageName());
+                    Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                    goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                    try {
+                        startActivity(goToMarket);
+                    } catch (ActivityNotFoundException e) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + getActivity().getPackageName())));
+                    }
+                    Toast.makeText(getActivity(), "You rock! :)",
+                            Toast.LENGTH_LONG).show();
+                    return true;
+                }
+            });
+
+            changelog.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    ChangelogDialog dialog = ChangelogDialog.newInstance();
+                    dialog.show(fm,"change");
+                    return true;
+                }
+            });
+
+            licenses.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    LicensesFragment dialog = LicensesFragment.newInstance();
+                    dialog.show(fm,"licenses");
+                    return true;
+                }
+            });
         }
 
     }
